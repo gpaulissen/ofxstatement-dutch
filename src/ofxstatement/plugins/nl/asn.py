ed59ff15c260a25e0ef03b101e700de982627676
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2022-07-18 19:51:30 +0200

    Testing asn module.

--- src/ofxstatement/plugins/nl/asn.py
+++ src/ofxstatement/plugins/nl/asn.py
@@ -6,6 +6,7 @@ import csv
 import sys
 import datetime
 import logging
+from decimal import Decimal
 
 from ofxstatement.plugin import Plugin as BasePlugin
 from ofxstatement.parser import CsvStatementParser
@@ -23,15 +24,6 @@ logger.addHandler(logging.NullHandler())
 
 class Parser(CsvStatementParser):
     """
-
-    These are the first two lines of an ING Netherlands CSV file:
-
-    "Datum","Naam / Omschrijving","Rekening","Tegenrekening","Code",\
-"Af Bij","Bedrag (EUR)","MutatieSoort",\
-"Mededelingen"
-    "20200213","Kosten OranjePakket met korting","NL42INGB0001085276","","DV",\
-"Af","1,25","Diversen",\
-"1 jan t/m 31 jan 2020 ING BANK N.V. Valutadatum: 13-02-2020"
 
     These fields are from the Statement class:
 
@@ -84,54 +76,36 @@ class Parser(CsvStatementParser):
 
     """
 
-    date_format: str
+    date_format: str = "%d-%m-%Y"
 
-    # transactions / balance
-    header: List[List[str]] = [["Datum",
-                                "Naam / Omschrijving",
-                                "Rekening",
-                                "Tegenrekening",
-                                "Code",
-                                "Af Bij",
-                                "Bedrag (EUR)",
-                                "MutatieSoort",
-                                "Mededelingen"],
-                               ["Datum",
-                                "Boeksaldo",
-                                "Valutair saldo"]]
     # 0-based
-    mappings_by_header: List[Dict[str, int]] = [{
+    mappings: Dict[str, int] = {
         # id (determined later)
         'date': 0,
-        'memo': 8,
-        'amount': 6,
-        'payee': 1,  # if bank_account_to is filled
+        'memo': 17,
+        'start_balance': 8,
+        'amount': 10,
+        'payee': 3,  # if bank_account_to is filled
         # date_user
         # check_no
         # refnum
         # trntype (determined later)
-        'bank_account_to': 3,
-    }, {
-        'date': 0,
-        'amount': 2  # valutair
-    }]
+        'bank_account_to': 2,
+    }
 
     # variables
     unique_id_set: Set[str]
-    header_idx: int
-    mappings: Dict[str, int]
 
     def __init__(self,
                  fin: Iterable[str],
                  account_id: Optional[str] = None) -> None:
         # Python 3 needed
         super().__init__(fin)
-        # Use the BIC code for ING Netherlands
-        self.statement = Statement(bank_id="INGBNL2A",
+        # Use the BIC code for ASN Bank
+        self.statement = Statement(bank_id="ASNBNL21",
                                    account_id=account_id,
                                    currency="EUR")  # My Statement
         self.unique_id_set = set()
-        self.header_idx = -1
 
     def parse(self) -> Statement:
         """Main entry point for parsers
@@ -143,34 +117,16 @@ class Parser(CsvStatementParser):
         # Python 3 needed
         stmt: Statement = super().parse()
 
-        try:
-            assert self.header_idx >= 0 and self.header_idx < len(self.header),\
-                "Header not read: {}".format(str(self.header))
-        except Exception as e:
-            raise ParseError(0, str(e))
+        # GJP 2020-03-03
+        # No need to (re)calculate the balance since there is no history.
+        # But set the dates.
+        stmt.start_date = min(sl.date for sl in stmt.lines)
+        # end date is exclusive for OFX
+        stmt.end_date = max(sl.date for sl in stmt.lines)
+        stmt.end_date += datetime.timedelta(days=1)
 
-        if self.header_idx == 0:
-            # GJP 2020-03-03
-            # No need to (re)calculate the balance since there is no history.
-            # But set the dates.
-            stmt.start_balance = stmt.end_balance = None
-            stmt.start_date = min(sl.date for sl in stmt.lines)
-            # end date is exclusive for OFX
-            stmt.end_date = max(sl.date for sl in stmt.lines)
-            stmt.end_date += datetime.timedelta(days=1)
-        elif self.header_idx == 1:
-            stmt.start_date = stmt.start_balance = None
-
-            stmt.end_date = max(sl.date for sl in stmt.lines)
-            assert stmt.lines[0].date == stmt.end_date or \
-                stmt.lines[-1].date == stmt.end_date
-            end_idx: int = 0 if stmt.lines[0].date == stmt.end_date else -1
-            stmt.end_balance = stmt.lines[end_idx].amount
-            # end date is exclusive for OFX
-            stmt.end_date += datetime.timedelta(days=1)
-
-            # no transaction lines
-            stmt.lines = []
+        stmt.start_balance = Decimal(stmt.lines[0].start_balance)
+        stmt.end_balance = Decimal(stmt.lines[-1].start_balance) + stmt.lines[-1].amount
 
         return stmt
 
@@ -187,32 +143,11 @@ class Parser(CsvStatementParser):
         stmt_line: Optional[StatementLine] = None
 
         try:
-            logger.debug('header idx: %d; line #%d: %s',
-                         self.header_idx,
+            logger.debug('line #%d: %s',
                          self.cur_record,
                          line)
 
-            # First record(s) must be the header
-            if self.header_idx < 0:
-                if line == self.header[0]:
-                    self.header_idx = 0
-                    self.date_format = "%Y%m%d"
-                elif line == self.header[1]:
-                    self.header_idx = 1
-                    self.date_format = "%Y-%m-%d"
-
-                msg: str = "Line {} does not match\n\n{}\n\nnor\n\n{}\n"
-
-                assert self.header_idx in [0, 1],\
-                    msg.format(line, self.header[0], self.header[1])
-
-                self.mappings = self.mappings_by_header[self.header_idx]
-                return None
-
-            if self.header_idx == 0:
-                stmt_line = self.parse_transaction(line)
-            elif self.header_idx == 1:
-                stmt_line = self.parse_balance(line)
+            stmt_line = self.parse_transaction(line)
 
         except Exception as e:
             raise ParseError(self.cur_record, str(e))
@@ -221,29 +156,19 @@ class Parser(CsvStatementParser):
 
     def parse_transaction(self,
                           line: List[Optional[str]]) -> Optional[StatementLine]:
-        # line[2] contains the account number
+        # line[1] contains the account number
         if self.statement.account_id:
-            assert self.statement.account_id == line[2],\
+            assert self.statement.account_id == line[1],\
                 "Only one account is allowed; previous account: {}, \
-this line's account: {}".format(self.statement.account_id, line[2])
+this line's account: {}".format(self.statement.account_id, line[1])
         else:
-            self.statement.account_id = line[2]
-
-        assert line[5] in ['Af', 'Bij']
-
-        if line[5] == 'Af':
-            amount: Optional[str] = line[self.mappings['amount']]
-            line[self.mappings['amount']] =\
-                '-' + (amount if isinstance(amount, str) else '')
+            self.statement.account_id = line[1]
 
         if line[self.mappings['bank_account_to']]:
             line[self.mappings['payee']] =\
                 "{} ({})".format(line[self.mappings['payee']],
                                  line[self.mappings['bank_account_to']])
         else:
-            line[self.mappings['memo']] =\
-                "{}, {}".format(line[self.mappings['payee']],
-                                line[self.mappings['memo']])
             line[self.mappings['payee']] = None
 
         # Python 3 needed
@@ -254,7 +179,7 @@ this line's account: {}".format(self.statement.account_id, line[2])
             return None
 
         # Determine some fields not in the self.mappings
-        # A hack but needed to use the adjust method
+        # A hack but needed to use the adjust methods
         stmt_line.__class__ = StatementLine
         stmt_line.adjust(self.unique_id_set)
 
@@ -267,29 +192,24 @@ this line's account: {}".format(self.statement.account_id, line[2])
             stmt_line.bank_account_to = \
                 BankAccount(bank_id=None,
                             acct_id=stmt_line.bank_account_to)
-        return stmt_line
+        else:
+            stmt_line.bank_account_to = None
 
-    def parse_balance(self,
-                      line: List[Optional[str]]) -> Optional[StatementLine]:
-        # Python 3 needed
-        stmt_line: StatementLine = super().parse_record(line)
-        stmt_line.trntype = "DEBIT" if stmt_line.amount < 0 else "CREDIT"
-        stmt_line.id = 1
-        # Determine some fields not in the self.mappings
-        # A hack but needed to use the adjust method
-        stmt_line.__class__ = StatementLine
-        stmt_line.adjust(self.unique_id_set)
+        # strip quotes around memo
+        if len(stmt_line.memo) >= 2 and stmt_line.memo[0] == "'" and stmt_line.memo[-1] == "'":
+            stmt_line.memo = stmt_line.memo[1:-1]
+
         return stmt_line
 
 
 class Plugin(BasePlugin):
-    """ING Bank, The Netherlands, CSV (https://www.ing.nl/)
+    """ASN Bank, The Netherlands, CSV (https://www.asnbank.nl/)
     """
     def get_parser(self, filename: str) -> Parser:
-        p = re.compile('(NL\\d+INGB\\d+)')
+        p = re.compile('transactie-historie_(NL\\d+ASNB\\d+)_\\d+\\.csv')
         m = p.search(filename)
         account_id: Optional[str] = None
         if m:
-            account_id = m.group(0)
-        fin = open(filename, "r", encoding="ISO-8859-1")
+            account_id = m.group(1)
+        fin = open(filename, "r")  # , encoding="ISO-8859-1")
         return Parser(fin, account_id)
---
+++ tests/test_asn.py
@@ -0,0 +1,42 @@
+import os
+from unittest import TestCase
+from decimal import Decimal
+from datetime import datetime
+
+from ofxstatement.plugins.nl.asn import Plugin
+
+
+class ParserTest(TestCase):
+
+    def test_ok(self):
+        here = os.path.dirname(__file__)
+        text_filename = os.path.join(here, 'samples', 'transactie-historie_NL00ASNB9999999999_20220717204133.csv')
+        parser = Plugin(None, None).get_parser(text_filename)
+
+        # And parse csv:
+        statement = parser.parse()
+
+        self.assertEqual(statement.currency, 'EUR')
+        self.assertEqual(statement.bank_id, "ASNBNL21")
+        self.assertEqual(statement.account_id, "NL00ASNB9999999999")
+        self.assertEqual(statement.account_type, "CHECKING")
+
+        self.assertEqual(statement.start_balance, Decimal('130.44'))
+        self.assertEqual(statement.start_date,
+                         datetime.strptime("17-06-2022",
+                                           parser.date_format))
+
+        self.assertEqual(statement.end_balance, Decimal('644.24') + Decimal('-560.00'))
+        self.assertEqual(statement.end_date,
+                         datetime.strptime("12-07-2022",  # +1
+                                           parser.date_format))
+
+        self.assertEqual(len(statement.lines), 11)
+        self.assertEqual(statement.lines[0].amount, Decimal('223.77'))
+        self.assertEqual(statement.lines[0].bank_account_to.acct_id, 'NL99ASNB0000000000')
+        self.assertEqual(statement.lines[0].payee, 'XXXXXXXXX Z Z Z Z (NL99ASNB0000000000)')
+
+        self.assertIsNone(statement.lines[2].bank_account_to)
+        self.assertIsNone(statement.lines[2].payee)
+        self.assertEqual(statement.lines[2].memo,
+                         "Kosten gebruik betaalrekening inclusief 1 betaalpas")
